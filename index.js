3938fcb4134cdebe3d2f0ca8be9623232abf667e
TovarischPootis <54110479+TovarischPootis@users.noreply.github.com>
2023-03-25 22:51:06 +0100

    dsds

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+// fetch data from https://discord.com/api/guilds/787688375838703626/widget.json without discord.js
+
+/* var obj;
+
+window.addEventListener('load', () => {
+    fetch('https://discord.com/api/guilds/787688375838703626/widget.json')
+        .then(res => res.json())
+        .then(data => {
+            obj = data;
+        })
+        .then(() => {
+            console.log(obj);
+        });
+}); */
+
+// async function that returns a number from a fetch request.
+
+const noBots = 2;
+
+var onlineUsers;
+async function getOnline() {
+    const response = await fetch('https://discord.com/api/guilds/787688375838703626/widget.json');
+    const data = await response.json();
+    console.log(data);
+    onlineUsers = (data.presence_count - noBots);
+    //return data.presence_count;
+}
+
+getOnline();
+
+// any element with id 'k' gets their innerHTML set to the number returned from getOnline()
+
 var Typer = {
     text: '',
     accessCountimer: null,
@@ -26,19 +58,19 @@ var Typer = {
     },
 
     addText: function (key) {
-        if (key.key == 'alt') {
+        if (key.key == 'Alt') {
             Typer.accessCount++;
 
             if (Typer.accessCount >= 3) {
                 Typer.makeAccess();
             }
-        } else if (key.key == 'caps lock') {
+        } else if (key.key == 'CapsLock') {
             Typer.deniedCount++;
 
             if (Typer.deniedCount >= 3) {
                 Typer.makeDenied();
             }
-        } else if (key.key == 'escape') {
+        } else if (key.key == 'Escape') {
             Typer.hidepop();
         } else if (Typer.text) {
             var cont = Typer.content();
@@ -48,7 +80,7 @@ var Typer = {
                         .html()
                         .substring(0, cont.length - 1),
                 );
-            if (key.key != 'backspace') {
+            if (key.key != 'Backspace') {
                 Typer.index += Typer.speed;
             } else {
                 if (Typer.index > 0) Typer.index -= Typer.speed;
@@ -57,14 +89,14 @@ var Typer = {
             var rtn = new RegExp('\n', 'g');
 
             $('#console').html(text.replace(rtn, '<br/>'));
-            window.scrollBy(0, 50);
+            window.scrollBy(0, 50)
         }
 
-        if (key.preventDefault && key.key != 'f11') {
+        if (key.preventDefault && key.key != 'F11') {
             key.preventDefault();
         }
 
-        if (key.key != 'f11') {
+        if (key.key != 'F11') {
             // otherway prevent keys default behavior
             key.returnValue = false;
         }
@@ -102,6 +134,11 @@ Typer.init();
 var timer = setInterval('t();', 30);
 function t() {
     Typer.addText({keyCode: 123748});
+    var kThing = document.getElementById('k');
+
+    if (kThing) {
+        kThing.innerHTML = onlineUsers;
+    }
 
     if (Typer.index > Typer.text.length) {
         clearInterval(timer);
@@ -110,7 +147,7 @@ function t() {
 }
 
 document.onkeydown = function (e) {
-    if (e.key == "escape") {
+    if (e.key == "Escape") {
         // fastforward text
         Typer.index = Typer.text.length;
     }
